a36d4687454ffcf467093de40d8efb0f5ae506a8
abdulla <abd@abd.abd>
2023-10-29 20:11:31 +0500

    changed alias and url

--- assets/Films-ed8cb5ad.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./ListContent-c2d24e59.js";import{o as a,c as o,a as s}from"./index-727cb2fe.js";const t={class:"main"},i={__name:"Films",setup(c){return(_,m)=>(a(),o("main",t,[s(e,{type:"movie"})]))}};export{i as default};
--- assets/Search-fddb0070.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as c,c as r}from"./index-727cb2fe.js";const o={};function t(n,a){return c(),r("div")}const f=e(o,[["render",t]]);export{f as default};
