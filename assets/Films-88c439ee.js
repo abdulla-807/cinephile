5ffed64c79aa423c425ab1f534506f1711891602
abdulla <abd@abd.abd>
2023-10-29 20:21:24 +0500

    changed alias and url

--- assets/Films-88c439ee.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./ListContent-904b1f5c.js";import{o as a,c as o,a as s}from"./index-32d540f6.js";const t={class:"main"},i={__name:"Films",setup(c){return(_,m)=>(a(),o("main",t,[s(e,{type:"movie"})]))}};export{i as default};
