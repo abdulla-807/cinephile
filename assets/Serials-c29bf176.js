2e6cf98084e1eaf7c7abc8b0ce982b6815546e21
abdulla <abd@abd.abd>
2023-10-29 20:17:23 +0500

    changed alias and url

---
+++ assets/Serials-c29bf176.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ListContent-904b1f5c.js";import{o as a,c as t,a as s}from"./index-32d540f6.js";const o={class:"main"},i={__name:"Serials",setup(c){return(r,_)=>(a(),t("main",o,[s(e,{type:"tv"})]))}};export{i as default};
