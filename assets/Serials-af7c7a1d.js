7cef09026f4ae0c5fae410884ba06a73ee61cb84
abdulla <abd@abd.abd>
2023-10-29 19:35:14 +0500

    builded alias

--- assets/Serials-af7c7a1d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./ListContent-061fea0c.js";import{o as a,c as t,a as s}from"./index-af0304c7.js";const o={class:"main"},i={__name:"Serials",setup(c){return(r,_)=>(a(),t("main",o,[s(e,{type:"tv"})]))}};export{i as default};
